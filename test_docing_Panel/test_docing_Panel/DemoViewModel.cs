f1e4dbfd08086ed18fbafce237df037a4c38e4e0
Drew Dowling <drew@quarticsolutions.com>
2019-08-26 14:55:05 -0700

    added phse 2 testing solution

---
+++ test_docing_Panel/test_docing_Panel/DemoViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+
+
+namespace test_docing_Panel
+{
+    internal class DemoViewModel : DockPane
+    {
+        private const string _dockPaneID = "test_docing_Panel_Demo";
+        private const string _menuID = "test_docing_Panel_Demo_Menu";
+
+        protected DemoViewModel() { }
+
+        /// <summary>
+        /// Show the DockPane.
+        /// </summary>
+        internal static void Show()
+        {
+            DockPane pane = FrameworkApplication.DockPaneManager.Find(_dockPaneID);
+            if (pane == null)
+                return;
+
+            pane.Activate();
+        }
+
+        /// <summary>
+        /// Text shown near the top of the DockPane.
+        /// </summary>
+        private string _heading = "My DockPane";
+        public string Heading
+        {
+            get { return _heading; }
+            set
+            {
+                SetProperty(ref _heading, value, () => Heading);
+            }
+        }
+
+        #region Burger Button
+
+        /// <summary>
+        /// Tooltip shown when hovering over the burger button.
+        /// </summary>
+        public string BurgerButtonTooltip
+        {
+            get { return "Options"; }
+        }
+
+        /// <summary>
+        /// Menu shown when burger button is clicked.
+        /// </summary>
+        public System.Windows.Controls.ContextMenu BurgerButtonMenu
+        {
+            get { return FrameworkApplication.CreateContextMenu(_menuID); }
+        }
+        #endregion
+    }
+
+    /// <summary>
+    /// Button implementation to show the DockPane.
+    /// </summary>
+    internal class Demo_ShowButton : Button
+    {
+        protected override void OnClick()
+        {
+            DemoViewModel.Show();
+        }
+    }
+
+    /// <summary>
+    /// Button implementation for the button on the menu of the burger button.
+    /// </summary>
+    internal class Demo_MenuButton : Button
+    {
+        protected override void OnClick()
+        {
+        }
+    }
+}
