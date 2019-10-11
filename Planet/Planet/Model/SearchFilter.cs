7f0909f37b8b9fd91b10d222cb818271157487d1
Drew Dowling <drew@quarticsolutions.com>
2019-10-11 15:43:44 -0700

    nuget update

---
+++ Planet/Planet/Model/SearchFilter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_docing_Panel.Models
+{
+    public class SearchFilter
+    {
+        public Filter filter { get; set; }
+        public string[] item_types { get; set; }
+    }
+
+    public class Filter
+    {
+        public string type { get; set; }
+        public Config[] config { get; set; }
+    }
+
+    public class Config
+    {
+        public string type { get; set; }
+        public string field_name { get; set; }
+        public object config { get; set; }
+        public object[] coordinates { get; set; }
+        public string gte { get; set; }
+        public string lte { get; set; }
+    }
+    public class DateRangeFilterConfig
+    {
+        public string gte { get; set; }
+        public string lte { get; set; }
+    }
+    public class RangeFilterConfig
+    {
+        public double gte { get; set; }
+        public double lte { get; set; }
+    }
+
+    public class RangeFilter
+    {
+        public string type { get; set; }
+        public string field_name { get; set; }
+        public RangeFilterConfig config { get; set; }
+
+    }
+
+}
