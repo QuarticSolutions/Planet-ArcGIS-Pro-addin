0f57bbc817548c077ce76416e45da00fbc6631a0
Drew Dowling <drew@quarticsolutions.com>
2019-12-06 12:24:36 -0800

    About window converted to a dropdown

--- Planet/Planet/PlanetConnection.cs
+++ Planet/Planet/PlanetConnection.cs
@@ -369,4 +369,26 @@ namespace Planet
             _action((PasswordBox)parameter);
         }
     }
+    public class CommandHandlerParam : ICommand
+    {
+        private Action<object> _action;
+        private bool _canExecute;
+        public CommandHandlerParam(Action<object> action, bool canExecute)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _action((object)parameter);
+        }
+    }
 }
---
+++ Planet/Planet/Tools/InformationGallery.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+using System.Windows.Input;
+using System.Windows.Documents;
+using Planet.ViewModel;
+
+namespace Planet
+{
+    internal class InformationGallery : Gallery
+    {
+        private bool _isInitialized;
+
+        protected override void OnDropDownOpened()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            if (_isInitialized)
+                return;
+            InformationItem informationItem = new InformationItem();
+            Add(informationItem);
+            //Add 6 items to the gallery
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    string name = string.Format("Item {0}", i);
+            //    Add(new GalleryItem(name, this.LargeImage != null ? ((ImageSource)this.LargeImage).Clone() : null, name));
+            //}
+            _isInitialized = true;
+
+        }
+
+        protected override void OnClick(GalleryItem item)
+        {
+            //TODO - insert your code to manipulate the clicked gallery item here
+            //System.Diagnostics.Debug.WriteLine("Remove this line after adding your custom behavior.");
+            //base.OnClick(item);
+        }
+
+ 
+
+
+
+    }
+
+}
